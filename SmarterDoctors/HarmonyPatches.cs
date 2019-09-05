bb2e44326d1fa3fad312881aa29ee65cf7766250
Chris Lewark <chris.lewark@gmail.com>
2019-09-05 19:08:21 -0400

    First implementation

--- SmarterDoctors/HarmonyPatches.cs
+++ SmarterDoctors/HarmonyPatches.cs
@@ -14,6 +14,7 @@ namespace SmarterDoctors
     {
         static Main()
         {
+            //Log.Message("Hello from Harmony in scope: com.github.harmony.rimworld.maarx.smarterdoctors");
             var harmony = HarmonyInstance.Create("com.github.harmony.rimworld.maarx.smarterdoctors");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
@@ -21,23 +22,86 @@ namespace SmarterDoctors
 
     class Computations
     {
-        public static float computePriority(Pawn p)
+        public static float computePriority(Pawn worker, Pawn target)
         {
-            return 1f;
+            bool isSick = target.health.hediffSet.HasImmunizableNotImmuneHediff();
+            int ticksToBleedOut = HealthUtility.TicksUntilDeathDueToBloodLoss(target);
+            float delta = float.MaxValue;
+            float priority = 0;
+            if (isSick)
+            {
+                delta = FindMostSevereHediffDelta(target);
+                priority = ((float) Math.Pow(10, 20)) + delta;
+            }
+            else if (ticksToBleedOut < int.MaxValue)
+            {
+                priority = ((float)Math.Pow(10, 10)) - ticksToBleedOut;
+            }
+            //Log.Message("Hello from computePriority with pawn: " + target.Name.ToStringShort + ", priority: " + priority);
+
+            return priority;
+        }
+
+        //https://github.com/Mehni/kNumbers/blob/fc9320b45bc0a8c41b2f2c67f68107fb89e84848/Numbers/PawnColumnWorkers/PawnColumnWorker_DiseaseProgression.cs#L152
+        private static float FindMostSevereHediffDelta(Pawn pawn)
+        {
+            IEnumerable<HediffWithComps> tmplist =
+                pawn.health.hediffSet.hediffs.Where(x => x.Visible && x is HediffWithComps && !x.FullyImmune()).Cast<HediffWithComps>();
+
+            float delta = float.MinValue;
+            HediffWithComps mostSevereHediff = null;
+
+            foreach (HediffWithComps hediff in tmplist)
+            {
+                HediffComp_Immunizable hediffCompImmunizable = hediff.TryGetComp<HediffComp_Immunizable>();
+
+                if (hediffCompImmunizable == null)
+                    continue;
+
+                if (hediffCompImmunizable.Immunity - hediff.Severity > delta)
+                {
+                    delta = hediffCompImmunizable.Immunity - hediff.Severity;
+                    mostSevereHediff = hediff;
+                }
+            }
+
+            return delta;
         }
     }
 
-    [HarmonyPatch(typeof(WorkGiver_Tend))]
+    [HarmonyPatch(typeof(WorkGiver_Scanner))]
     [HarmonyPatch("GetPriority")]
     [HarmonyPatch(new Type[] { typeof(Pawn), typeof(TargetInfo) })]
-    class ThingLabel
+    class Patch_WorkGiver_Scanner_GetPriority
     {
-        static void Postfix(WorkGiver_Tend __instance, ref Pawn pawn, ref TargetInfo t, ref float __result)
+        static void Postfix(WorkGiver_Scanner __instance, ref Pawn pawn, ref TargetInfo t, ref float __result)
         {
-            Log.Message("Hello from Harmony WorkGiver_Tend GetPriority Postfix with result:" + __result);
-            if (__result == 0f)
+            //Log.Message("Hello from Harmony WorkGiver_Scanner GetPriority Postfix with type: " + __instance.GetType());
+            if (__instance is WorkGiver_Tend)
             {
-                __result = Computations.computePriority((Pawn) t.Thing);
+                //Log.Message("Hello from Harmony WorkGiver_Tend GetPriority Postfix with type: " + __instance.GetType());
+                //Log.Message("Hello from Harmony WorkGiver_Tend GetPriority Postfix with old result:" + __result);
+                if (__result == 0f)
+                {
+                    __result = Computations.computePriority(pawn, (Pawn)t.Thing);
+                }
+                //Log.Message("Bybye from Harmony WorkGiver_Tend GetPriority Postfix with new result:" + __result);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(WorkGiver_Scanner))]
+    [HarmonyPatch("Prioritized", MethodType.Getter)]
+    class Patch_WorkGiver_Scanner_Prioritized
+    {
+        static void Postfix(WorkGiver_Scanner __instance, ref bool __result)
+        {
+            //Log.Message("Hello from Harmony WorkGiver_Scanner Prioritized Postfix with type: " + __instance.GetType());
+            if (__instance is WorkGiver_Tend)
+            {
+                //Log.Message("Hello from Harmony WorkGiver_Tend Prioritized Postfix with result:" + __result);
+                __result = true;
+                //Log.Message("Goodbye from Harmony WorkGiver_Tend Prioritized Postfix with result:" + __result);
             }
         }
     }
